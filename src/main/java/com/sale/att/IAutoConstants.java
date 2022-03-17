ac35e47cb077f4731ebf84d4c848836829409c7b
VIKKY-123 <vikky2490@gamail.com>
2022-03-17 21:35:11 +0530

    first commit

---
+++ src/main/java/com/sale/att/IAutoConstants.java
@@ -0,0 +1,15 @@
+package com.sale.att;
+
+
+public interface IAutoConstants {
+	
+	String dirpath=System.getProperty("user.dir");
+	
+	String pro_path= dirpath+"/common.properties";
+	
+	
+	String ex_path=dirpath+"/Excel.xlsx";
+	
+	
+
+}
